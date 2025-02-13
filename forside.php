64f74fd49918b03b7a2a80f54bd9b3b8bf9da70b
emmaprobst <emp003@edu.zealand.dk>
2025-02-13 09:32:30 +0100

    forside added

---
+++ forside.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Forside til kunde</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body style="background-image: url('images/backgroundImage.png');
+             background-size: cover;
+             background-repeat: no-repeat;
+             min-height: 100vh;"
+      class="d-flex flex-column">
+
+
+
+<div class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <button class="btn-custom py-5 btn btn-orange rounded-5 fs-3">NY BESTILLING</button>
+</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
