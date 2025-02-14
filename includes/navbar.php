5727843195b8c486b242b9165bf5dc39561e5a3e
emiliekolthoff <emk006@edu.zealand.dk>
2025-02-14 13:05:53 +0100

    rettelse af farve forvirring

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,8 +16,7 @@
 
     <link rel="icon" type="image/x-icon" href="images/croissantLogo2.png">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body>
