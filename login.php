4a81b6bc51c05582b3286f138158cb790a5aa75a
emmaprobst <emp003@edu.zealand.dk>
2025-02-14 10:33:16 +0100

    Har opstartet undersiden med login til bager-delen

--- login.php
+++ login.php
@@ -1,1 +1,37 @@
 <?php
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Hansens Bageri</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/30d6e0b537.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/x-icon" href="images/croissantFavicon.png" class="">
+
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
