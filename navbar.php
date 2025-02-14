cb83e0a84681f1709e20d2bdae7255672cfac336
louise <asfkls@mgrklm.dk>
2025-02-14 11:59:26 +0100

    mini crossaint logo tilføjet

--- navbar.php
+++ navbar.php
@@ -24,7 +24,7 @@
 <nav class="navbar bg-body-tertiary fixed-top">
     <div class="container-fluid">
         <a class="" href="#">
-            <img src="images/croissantLOGO.png" alt="logo" class="logoNavbar">
+            <img src="images/croissantLOGO_50.png" alt="logo" class="logoNavbar">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
