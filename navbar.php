9c9944731b886dd3cdbc3816ff5773ffac1c4c28
emiliekolthoff <emk006@edu.zealand.dk>
2025-02-14 12:26:19 +0100

    Dato tilføjet til navbar - mangler js rettelser :-(

--- navbar.php
+++ navbar.php
@@ -1,3 +1,6 @@
+<?php
+require "settings/init.php";
+?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
@@ -21,11 +24,26 @@
 
 <body>
 <!-- Nav bar -->
-<nav class="navbar bg-body-tertiary fixed-top">
+<nav class="navbar fixed-top">
     <div class="container-fluid">
         <a class="" href="#">
             <img src="images/croissantLOGO_50.png" alt="logo" class="logoNavbar">
         </a>
+
+        <div class="container w-50">
+            <div class="row align-items-center">
+                <div class="col">
+                    <button type="button" class="left btn bg-body-tertiary"><i class="fa-solid fa-arrow-left"></i></button>
+                </div>
+                <div class="col">
+                    <div class="date">I DAG</div>
+                </div>
+                <div class="col">
+                    <button type="button" class="right btn bg-body-tertiary"><i class="fa-solid fa-arrow-right"></i></button>
+                </div>
+            </div>
+        </div>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -59,4 +77,26 @@
 </body>
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    /*opgave med counter:*/
+    const right = document.querySelector(".right");
+    const left = document.querySelector(".left");
+    const date = document.querySelector(".date");
+
+
+    plus.addEventListener("click", () => {
+        const nyttal = parseInt(date.innerHTML) + 1;
+
+        if(nyttal <=11){
+            date.innerHTML = nyttal;
+        }
+
+    });
+    minus.addEventListener("click", () => {
+        const nyttal = parseInt(date.innerHTML) - 1;
+        if(nyttal>0){
+            date.innerHTML = nyttal;
+        }
+    });
+</script>
 </html>
