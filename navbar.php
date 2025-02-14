dfb9d73e1805a35187bf435f6f6a1ad3ba672eac
emiliekolthoff <emk006@edu.zealand.dk>
2025-02-14 11:19:23 +0100

    nav bar komponenter tilføjet

--- navbar.php
+++ navbar.php
@@ -1,1 +1,63 @@
-<?php
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Hansens Bageri</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/30d6e0b537.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/x-icon" href="images/croissantFavicon.png" class="">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<!-- Nav bar -->
+<nav class="navbar bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+        <a class="" href="#">
+            <img src="images/croissantLOGO.png" alt="logo" class="logoNavbar">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+<!-- content -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Hansens Bageri</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active m-2 text-center" aria-current="page" href="#">Ny Bestilling</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active m-2 text-center" aria-current="page" href="#">Se Bestilling</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active m-2 text-center" aria-current="page" href="#">Varer Lager</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active m-2 text-center" aria-current="page" href="#">Varer Bestilling</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active m-2 text-center" aria-current="page" href="#">Kundeservice</a>
+                    </li>
+            </div>
+        </div>
+    </div>
+</nav>
+
+</body>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</html>
