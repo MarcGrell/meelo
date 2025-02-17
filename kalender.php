39270c00b0f3586034628e1df4a1666e4c56542e
Marc <mah035@edu.zealand.dk>
2025-02-17 20:45:06 +0100

    Backend updated with PHP and functional!

--- kalender.php
+++ kalender.php
@@ -32,114 +32,76 @@ require "settings/init.php";
 <body>
 
 <?php
-include ("includes/navbar.php");
+include("includes/navbar.php");
 ?>
 
 
 <!--CARDS CONTAINER!-->
 <div class="container">
-    <div class="row align-items-start" id="cardContainer">
-        <!--HER KOMMER CARDS TIL AT VÆRE-->
-    </div>
-</div>
-
-<!--TEMPLATE FOR MODAL POPUP / BESTLLINGSPOPUP-->
-<div class="modal fade" id="popupModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Fornavn Efternavn</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Fuld beskrivelse af bestillingen...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">LUK</button>
-                <button type="button" class="btn btn-primary" data-dismiss="modal">PRINT</button>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pickupModal">AFHENT
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!--PICKUP TEMPLATE POPUP-->
-<div class="modal fade" id="pickupModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">AFHENT ORDER?</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Er du sikker?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-success" data-dismiss="modal">JA!</button>
-                <button type="button" class="btn btn-danger" data-dismiss="modal">NEJ!</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script>
-    // HER SKAL DATA FRA DATABASEN HENTES.....
-    const cardData = [
-        {name: "Lene Mikkelsen", description: "Kort beskrivelse af bestillingen."},
-        {name: "Emma - MEELO", description: "Kort beskrivelse af bestillingen."},
-        {name: "Mads Rasmussen", description: "Kort beskrivelse af bestillingen."},
-        {name: "Marc - AAUA", description: "Kort beskrivelse af bestillingen."},
-        {name: "Karsten - INSA TECH", description: "Kort beskrivelse af bestillingen."},
-        {name: "Glen Nielsen", description: "Kort beskrivelse af bestillingen."},
-
-    ];
-
-    // Get the container element where the cards will be appended
-    const cardContainer = document.getElementById("cardContainer");
-
-    // Use forEach to loop through the array and generate HTML
-    cardData.forEach((card, index) => {
-        const cardHTML = `
+    <div class="row">
+        <?php
+        $sql = "SELECT * FROM input ORDER BY inpuDate ASC";
+        $orders = $db->sql($sql);
+        $counter = 0; // Counter to track cards in the current row
+        foreach ($orders as $order) {
+            // Open a new row for every two cards
+            if ($counter % 2 == 0 && $counter != 0) {
+                echo '</div><div class="row">';
+            }
+            $counter++;
+            ?>
             <div class="col-6">
-                <div class="card mb-3" style="width: 18rem;">
+                <div class="card mb-3" style="width: 100%;">
                     <div class="card-body">
-                        <h5 class="card-title">${card.name}</h5>
-                        <p class="card-text">${card.description}</p>
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#popupModal${index}">
+                        <h5 class="card-title"><?php echo $order->inpuName; ?></h5>
+                        <p class="card-text"><strong>Kage type:</strong> <?php echo $order->inpuVar; ?></p>
+                        <p class="card-text"><strong>Antal personer:</strong> <?php echo $order->inpuPax; ?></p>
+                        <p class="card-text"><strong>Afhentes:</strong> <?php echo date('d-m-y H:i', strtotime($order->inpuDate)); ?></p>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#popupModal<?php echo $order->inpuId; ?>">
                             SE BESTILLING
                         </button>
 
                         <!-- Modal for each card -->
-                        <div class="modal fade" id="popupModal${index}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="popupModal<?php echo $order->inpuId; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                             aria-hidden="true">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">${card.name}</h5>
+                                        <h5 class="modal-title" id="exampleModalLabel">Ordre detaljer:</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
                                     <div class="modal-body">
-                                        Fuld beskrivelse af bestillingen...
+                                        <p class="card-text"><strong>Kage type:</strong> <?php echo $order->inpuVar; ?></p>
+                                        <p class="card-text"><strong>Kage form:</strong> <?php echo $order->inpuType; ?></p>
+                                        <p class="card-text"><strong>Antal personer:</strong> <?php echo $order->inpuPax; ?></p>
+                                        <p class="card-text"><strong>Slik:</strong> <?php echo $order->inpuCandy; ?></p>
+                                        <p class="card-text"><strong>Hår:</strong> <?php echo $order->inpuHair; ?></p>
+                                        <p class="card-text"><strong>Glasur:</strong> <?php echo $order->inpuIcing; ?></p>
+                                        <p class="card-text"><strong>Text:</strong> <?php echo $order->inpuText; ?></p>
+                                        <p class="card-text"><strong>Afhentes:</strong> <?php echo date('d-m-y H:i', strtotime($order->inpuDate)); ?></p>
+                                        <h4 class="card-text"><strong>Kundens oplysninger:</strong></h4>
+                                        <p class="card-text"><strong>Navn:</strong> <?php echo $order->inpuName; ?></p>
+                                        <p class="card-text"><strong>Telefon:</strong> <?php echo $order->inpuPhoneNumber; ?></p>
+                                        <p class="card-text"><strong>Email:</strong> <?php echo $order->inpuEmail; ?></p>
+                                        <p class="card-text"><strong>Firma:</strong> <?php echo $order->inpuCompany; ?></p>
+                                        <p class="card-text"><strong>Bemærkninger:</strong> <?php echo $order->inpuComment; ?></p>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-primary" data-dismiss="modal">LUK</button>
                                         <button type="button" class="btn btn-primary" data-dismiss="modal">PRINT</button>
-                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pickupModal${index}">AFHENT</button>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal"
+                                                data-target="#pickupModal<?php echo $order->inpuId; ?>">AFHENT
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
                         <!-- Pickup Modal for each card -->
-                        <div class="modal fade" id="pickupModal${index}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="pickupModal<?php echo $order->inpuId; ?>" tabindex="-1" role="dialog"
+                             aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
@@ -161,11 +123,13 @@ include ("includes/navbar.php");
                     </div>
                 </div>
             </div>
-        `;
+            <?php
+        }
+        ?>
+    </div>
+</div>
+<script>
 
-        // Append the generated card HTML to the card container
-        cardContainer.innerHTML += cardHTML;
-    });
 
 </script>
 
