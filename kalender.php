d21e633b625ea3f1ad94b2a7635d688c056af588
emmaprobst <emp003@edu.zealand.dk>
2025-02-18 08:53:51 +0100

    Har rettet farver og placering på knapper samt margin og padding for show :))))

--- kalender.php
+++ kalender.php
@@ -35,6 +35,7 @@ require "settings/init.php";
 include("includes/navbar.php");
 ?>
 
+<div class="my-4"></div>
 
 <!--CARDS CONTAINER!-->
 <div class="container">
@@ -56,7 +57,7 @@ include("includes/navbar.php");
                         <h5 class="card-title"><?php echo $order->inpuName; ?></h5>
                         <p class="card-text"><strong>Kagetype:</strong> <?php echo $order->inpuVar; ?></p>
                         <p class="card-text"><strong>Antal personer:</strong> <?php echo $order->inpuPax; ?></p>
-                        <p class="card-text"><strong>Afhentningsdag:</strong> <?php echo date('d-m-y H:i', strtotime($order->inpuDate)); ?></p>
+                        <p class="card-text"><strong>Afhentning:</strong> <?php echo date('d-m-y H:i', strtotime($order->inpuDate)); ?></p>
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#popupModal<?php echo $order->inpuId; ?>">
                             SE BESTILLING
                         </button>
@@ -67,8 +68,8 @@ include("includes/navbar.php");
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Oplysninger:</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <h4 class="modal-title fw-bold" id="exampleModalLabel">Oplysninger</h4>
+                                        <button type="button" class="close ms-auto bg-white border-0 fs-4" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
@@ -80,21 +81,23 @@ include("includes/navbar.php");
                                         <p class="card-text"><strong>Hår:</strong> <?php echo $order->inpuHair; ?></p>
                                         <p class="card-text"><strong>Glasur:</strong> <?php echo $order->inpuIcing; ?></p>
                                         <p class="card-text"><strong>Tekst:</strong> <?php echo $order->inpuText; ?></p>
-                                        <p class="card-text"><strong>Afhentningsdag:</strong> <?php echo date('d-m-y H:i', strtotime($order->inpuDate)); ?></p>
-                                        <h4 class="card-text"><strong>Kundens oplysninger:</strong></h4>
+                                        <p class="card-text"><strong>Afhentning:</strong> <?php echo date('d-m-y H:i', strtotime($order->inpuDate)); ?></p>
+                                        <h4 class="card-text mt-5 mb-3"><strong>Kundens oplysninger</strong></h4>
                                         <p class="card-text"><strong>Navn:</strong> <?php echo $order->inpuName; ?></p>
                                         <p class="card-text"><strong>Telefon:</strong> <?php echo $order->inpuPhoneNumber; ?></p>
                                         <p class="card-text"><strong>Email:</strong> <?php echo $order->inpuEmail; ?></p>
                                         <p class="card-text"><strong>Firma:</strong> <?php echo $order->inpuCompany; ?></p>
                                         <p class="card-text"><strong>Bemærkninger:</strong> <?php echo $order->inpuComment; ?></p>
                                     </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-primary text-end" data-dismiss="modal">LUK</button>
-                                        <button type="button" class="btn btn-primary" data-dismiss="modal">PRINT</button>
+                                    <div class="modal-footer d-flex justify-content-between">
+                                        <button type="button" class="btn btn-primary" data-dismiss="modal">LUK</button>
+                                    <div class="d-flex gap-2">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">PRINT</button>
                                         <button type="button" class="btn btn-secondary" data-toggle="modal"
                                                 data-target="#pickupModal<?php echo $order->inpuId; ?>">AFHENT
                                         </button>
                                     </div>
+                                </div>
                                 </div>
                             </div>
                         </div>
@@ -105,12 +108,12 @@ include("includes/navbar.php");
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">AFHENT ORDRE</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <h5 class="modal-title fw-bold" id="exampleModalLabel">AFHENT ORDRE</h5>
+                                        <button type="button" class="close ms-auto bg-white border-0 fs-4" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
-                                    <div class="modal-body">
+                                    <div class="modal-body fs-4">
                                         Er du sikker?
                                     </div>
                                     <div class="modal-footer">
@@ -128,10 +131,8 @@ include("includes/navbar.php");
         ?>
     </div>
 </div>
+
 <script>
 
-
 </script>
-
-
 </body>
